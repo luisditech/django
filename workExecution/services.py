1b18bdcb52d3bf48a2f8147fb52cc960d762bf85
luisditech <luis.contreras@ditech.es>
2025-07-17 15:39:55 +0200

    Delete workExecution directory

--- workExecution/services.py
+++
@@ -1,8 +0,0 @@
-from .models import WorkExecution
-
-def list_executions():
-    # return all executions, newest first
-    return WorkExecution.objects.all().order_by('-started_at')
-
-def get_execution(execution_id):
-    return WorkExecution.objects.get(pk=execution_id)
--- workExecution/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import WorkExecutionListAPIView, WorkExecutionDetailAPIView
-
-urlpatterns = [
-    path('workexecutions/', WorkExecutionListAPIView.as_view(), name='workexecution-list'),
-    path('workexecutions/<int:pk>/', WorkExecutionDetailAPIView.as_view(), name='workexecution-detail'),
-]
